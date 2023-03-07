78d644d7f532cd5bf21a63850cb082b896680b0a
Sofa <119440261+Sofia0603@users.noreply.github.com>
2023-03-08 00:04:30 +0400

    sa

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.scrollto a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 500,   // по умолчанию «400» 
+        easing: "linear" // по умолчанию «swing» 
+    });
+
+    return false;
+});
